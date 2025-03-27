20dfa1520f2beda82f1dec128336e80c5399f41c
Tanush T M <tanushtm.work@gmail.com>
2025-03-27 18:54:36 +0530

    football event removed

--- app/events/turf-cricket-showdown/page.js
+++ app/events/turf-cricket-showdown/page.js
@@ -68,63 +68,6 @@ export default function SportsEvents() {
           </Link>
         </div>
       </div>
-
-      <div className='max-w-3xl bg-gray-800 p-4 mt-12 rounded-2xl shadow-2xl border border-gray-700">'>
-        <h1 className="text-4xl font-bold text-center mt-12">Futsal Tournament</h1>
-
-        <p className="mt-4 text-gray-300 text-lg">
-          Fast, intense, and skill-driven—this five-a-side Futsal tournament is all about speed, strategy, and teamwork.
-          Played on a hard court with a smaller, faster ball, it promises high-energy matches and thrilling moments.
-          Whether you’re here to win or just for the love of the game, get ready to compete and showcase your skills!
-        </p>
-
-        <div className="mt-6 p-5 bg-gray-600 rounded-lg shadow-md">
-          <h2 className="text-2xl font-semibold">Participation Details</h2>
-          <p className="mt-2"><strong>Team Size:</strong> 5 players on the field with a rolling substitution format (5+2)</p>
-          <p><strong>Entry Fee:</strong> ₹ 300 per team (to be decided)</p>
-          <p><strong>Location:</strong> <Link href="https://maps.app.goo.gl/UjK3zErpRCWWijas8?g_st=aw" className='text-sky-500 hover:text-sky-700'>Turfvolution, Manapakkam</Link></p>
-          <p><strong>Date:</strong>March 29, 2025</p>
-          <p><strong>Time:</strong>10:00 AM - 3:00 PM</p>
-        </div>
-
-        <div className="mt-6">
-          <h2 className="text-2xl font-semibold">Event Flow</h2>
-          <ul className="list-disc list-inside mt-2 text-lg">
-            <li><strong>Round 1:</strong> League Stage or Knockout matches, depending on the number of team registrations.</li>
-            <li><strong>Semi-Finals & Finals:</strong> The top four teams will compete in the semi-finals, with the winners advancing to the final.</li>
-          </ul>
-        </div>
-
-        <div className="mt-6">
-          <h2 className="text-2xl font-semibold">Rules & Regulations</h2>
-          <ul className="list-disc list-inside mt-2 text-lg">
-            <li>Each team must have 5 players and a maximum of 2 substitutes.</li>
-            <li>Standard futsal rules apply, including a no-slide tackle rule.</li>
-            <li>Matches will have two halves of [5+1+5 mins] with a short break in between.</li>
-            <li>In case of a draw during knockout rounds, penalty shootouts will determine the winner.</li>
-            <li>The referee’s decision is final, and any misconduct will lead to disqualification.</li>
-            <li>No refund of registeration fee will be issued for any reason.</li>
-          </ul>
-        </div>
-
-        <div className="mt-6 p-5 bg-gray-600 rounded-lg shadow-md">
-          <h2 className="text-2xl font-semibold">Contact Information</h2>
-          <p className="mt-2"><strong>Name:</strong> Sundar</p>
-          <p><strong>Phone:</strong> 9940236047</p>
-          <p><strong>Email:</strong> rk.sundu21@gmail.com</p>
-          <p className="mt-2"><strong>Name:</strong> Sam Jefferson</p>
-          <p><strong>Phone:</strong> 8939771980</p>
-          <p><strong>Email:</strong> samjefferson2005@gmail.com</p>
-        </div>
-
-        <div className="mt-6 flex justify-center">
-          <Link href="https://docs.google.com/forms/d/e/1FAIpQLSeu8F7ez_HCKOaGe3_2D7ZrOMbH-gPXGvFEhBznKX5tdTPH9A/viewform?usp=sharing" target="_blank">
-            <button className="px-6 py-3 text-lg font-semibold text-white bg-blue-600 rounded-lg shadow-md transition-transform transform hover:scale-105 hover:bg-blue-700 active:scale-95">
-              Register Now
-            </button>
-          </Link>
-        </div>
-      </div>
     </div>
   );
 }
